1d83a9a3e06a50a8e1b01e7d039cd4bb2fe80066
Dheeraj <dtarachandani@tekion.com>
2019-08-08 17:02:30 +0530

    delete duplicate file

--- inputBox/index.js
+++
@@ -1,9 +0,0 @@
-document.querySelector('#rangeInput').addEventListener('change', handleInput);
-
-var enteredNumbers = [];
-
-function handleInput(evt) {
-  console.log(evt.target.value);
-}
-
-enteredNumbers
